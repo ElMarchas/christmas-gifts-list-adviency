da5d9d79a2b2697c263790139f547c1fb1f43967
ElMarchas <marchas53@hotmail.com>
2023-01-11 20:02:44 -0600

    Updates

---
+++ RandomGifts.jsx
@@ -0,0 +1,38 @@
+export const RANDOMGIFTS = [
+  {
+    gift: "Hugo Segovia Disc",
+    units: "1",
+    picture: "/hugo.jpg",
+    price: "500",
+  },
+  {
+    gift: "Joji Disc",
+    units: "1",
+    picture: "/joji.jpg",
+    price: "500",
+  },
+  {
+    gift: "Polistation 8",
+    units: "1",
+    picture: "/polistation.webp",
+    price: "80000",
+  },
+  {
+    gift: "Money",
+    units: "3",
+    picture: "/dolares.webp",
+    price: "9999",
+  },
+  {
+    gift: "Gotic Egirl Outfit",
+    units: "1",
+    picture: "/gotic.jpg",
+    price: "1200",
+  },
+  {
+    gift: "Tardis",
+    units: "8",
+    picture: "/tardis.jpg",
+    price: "3",
+  },
+];
